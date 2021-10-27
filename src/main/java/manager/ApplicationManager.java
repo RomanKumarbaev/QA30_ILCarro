57b7d1f6c7b448947020f35d459c3d15510d736f
Roman Kumarbaev <91917230+RomanKumarbaev@users.noreply.github.com>
2021-10-27 14:53:38 +0300

    HelperUser

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,31 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    WebDriver wd;
+    UserHelper userHelper;
+
+    public  void init(){
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.navigate().to("https://ilcarro.xyz/search");
+        wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    userHelper = new UserHelper(wd);
+
+
+
+    }
+
+    public void stop(){
+
+    }
+
+
+    public UserHelper getUserHelper() {
+        return userHelper;
+    }
+}
